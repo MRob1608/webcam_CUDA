e6cb44656581adab20d0842c6ffd31c0220d8178
MRob1608 <marcyrobotti@gmail.com>
2025-05-22 03:27:25 +0200

    Sistemata conversione immagini da RGB888 a RGB8888, modificato CMake per supportare compilazione con nvcc (TODO eliminare warning nella compilazione). Implementata conversione anche via GPU

--- interface.c
+++ interface.c
@@ -22,13 +22,13 @@ void init_x11(int width, int height) {
 
     XFlush(display);
 }
-
+/*
 void display_frame(unsigned char* rgb_data, int width, int height) {
     int bytes_per_pixel = 3;
-    int bytes_per_line = width * bytes_per_line;
+    int bytes_per_line = width * 3;
     if (!ximage) {
         ximage = XCreateImage(display, DefaultVisual(display, 0), 24,
-                              ZPixmap, 0, (char*)rgb_data, width, height, 32, bytes_per_line);
+                              ZPixmap, 0, (char*)rgb_data, width, height, 32, 0);
     } else {
         ximage->data = (char*)rgb_data;
     }
@@ -37,6 +37,34 @@ void display_frame(unsigned char* rgb_data, int width, int height) {
     XFlush(display);
     
 
+}
+*/
+
+void display_frame(unsigned char* rgb_data, int width, int height) {
+    int bytes_per_pixel = 4;
+    int bytes_per_line = width * bytes_per_pixel; // allineato a 4 byte
+    if (ximage) {
+        XDestroyImage(ximage); // importante: evita memory leak
+    }
+    int depth = DefaultDepth(display, DefaultScreen(display));
+
+    ximage = XCreateImage(display,
+                          DefaultVisual(display, 0),
+                          24,                 // depth (bit per pixel)
+                          ZPixmap,
+                          0,
+                          (char*)rgb_data,
+                          width,
+                          height,
+                          8,                  // bitmap_pad in bit
+                          bytes_per_line);    // bytes per line, esplicito!
+    if (!ximage) {
+        fprintf(stderr, "XCreateImage failed! width=%d height=%d depth=%d bytes_per_line=%d\n",
+                width, height, depth, bytes_per_line);
+        return;
+    }
+    XPutImage(display, window, gc, ximage, 0, 0, 0, 0, width, height);
+    XFlush(display);
 }
 
 
